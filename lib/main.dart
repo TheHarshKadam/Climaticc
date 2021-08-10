62e19b759febb421e1c9e5acc378b45f7bf87fbb
Harshvardhan Kadam <77354987+TheHarshKadam@users.noreply.github.com>
2021-08-11 00:23:10 +0530

    Added theme and Splash screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,33 +1,61 @@
 import 'package:flutter/material.dart';
+import 'package:climaticc/screens/home.dart';
+import 'package:flutter/services.dart';
+import 'dart:async';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+  
   @override
+  
   Widget build(BuildContext context) {
-    return MaterialApp(
-      
-    );
+        return MaterialApp(
+          theme: ThemeData(
+            scaffoldBackgroundColor: Color(0xFF121212),
+            appBarTheme: AppBarTheme(
+              color: Color(0xFF121212),
+              elevation: 0
+            ),
+            visualDensity: VisualDensity.adaptivePlatformDensity
+          ),
+          debugShowCheckedModeBanner: false,
+          home: splashScreen(),
+        );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  
-
+class splashScreen extends StatefulWidget {
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  _splashScreenState createState() => _splashScreenState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  
+class _splashScreenState extends State<splashScreen> {
+  void initState() {
+    super.initState();
+    Timer(
+        Duration(seconds: 2),
+        () => Navigator.pushReplacement(
+            context, MaterialPageRoute(builder: (context) => home())));
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-       
+    SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
+      statusBarColor: Color(0xFF121212),
+       )
     );
+    return Container(
+        color: Color(0xFF121212),
+        child: Center(
+            child: Image(
+          image: AssetImage(
+            'assets/icon.png',
+          ),
+          width: 150.0,
+          height: 150.0,
+        )));
   }
 }
