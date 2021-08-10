3e155f2d28b151cc000758d49b3a7e6f1240e229
Harshvardhan Kadam <77354987+TheHarshKadam@users.noreply.github.com>
2021-08-10 23:00:47 +0530

    Add files via upload

---
+++ climaticc/lib/main.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+       
+    );
+  }
+}
