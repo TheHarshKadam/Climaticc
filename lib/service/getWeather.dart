12dd1c839ddc535e09b8cabc8cb24b9c7ef13d5f
Harshvardhan Kadam <77354987+TheHarshKadam@users.noreply.github.com>
2021-08-14 01:25:07 +0530

    functional, conditional changes

--- lib/service/getWeather.dart
+++ lib/service/getWeather.dart
@@ -4,6 +4,7 @@ import 'package:climaticc/service/location.dart';
 
 const apiKey = '681bd9aba1e6308c5cddc42c6e66e21a';
 const weatherApiLink = 'api.openweathermap.org/data/2.5/weather';
+const myCity = 'Navi Mumbai';
 
 class NetworkHelper {
   NetworkHelper(this.url);
@@ -27,35 +28,42 @@ class Weather {
     var weatherData = await networkHelper.getData();
     return weatherData;
   }
-}
 
-Future<dynamic> userCurrentLocation() async {
-  currentLocation location = currentLocation();
-  await location.getCurrentLocation();
-  NetworkHelper networkHelper = NetworkHelper(
-      '$weatherApiLink?lat=${location.latitude}&lon=${location.longitude}&appid=$apiKey');
-  var weatherData = await networkHelper.getData();
-  return weatherData;
-}
+  Future<dynamic> userCurrentLocation() async {
+    currentLocation location = currentLocation();
+    await location.getCurrentLocation();
+    NetworkHelper networkHelper = NetworkHelper(
+        '$weatherApiLink?lat=${location.latitude}&lon=${location.longitude}&appid=$apiKey');
+    var weatherData = await networkHelper.getData();
+    return weatherData;
+  }
 
-String getWeatherIcon(int conditions) {
-  if (conditions < 300) {
-    return 'thunder';
-  } else if (conditions < 400) {
-    return 'drizzle';
-  } else if (conditions == 500) {
-    return 'rain';
-  } else if (conditions < 600) {
-    return 'heavy_rain';
-  } else if (conditions < 700) {
-    return 'snow';
-  } else if (conditions < 800) {
-    return 'fog';
-  } else if (conditions == 800) {
-    return 'sun';
-  } else if (conditions <= 804) {
-    return 'cloud';
-  } else {
-    return 'thermometer';
+  Future<dynamic> myCityWeather() async {
+    NetworkHelper networkHelper =
+        NetworkHelper('$weatherApiLink?q=$myCity&appid=$apiKey&units=metric');
+    var weatherData = await networkHelper.getData();
+    return weatherData;
+  }
+
+  String getWeatherIcon(int conditions) {
+    if (conditions < 300) {
+      return 'thunder';
+    } else if (conditions < 400) {
+      return 'drizzle';
+    } else if (conditions == 500) {
+      return 'rain';
+    } else if (conditions < 600) {
+      return 'heavy_rain';
+    } else if (conditions < 700) {
+      return 'snow';
+    } else if (conditions < 800) {
+      return 'fog';
+    } else if (conditions == 800) {
+      return 'sun';
+    } else if (conditions <= 804) {
+      return 'cloud';
+    } else {
+      return 'thermometer';
+    }
   }
 }
